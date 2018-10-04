ed30a5cda3874949a60df4d1e73c528a5bf91eb6
Bruno Teixeira <bteixeira666@gmail.com>
2018-10-04 18:15:18 +0200

    Added some static content.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/', express.static(path.resolve(__dirname, '../public')));
+app.get('/hello', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
