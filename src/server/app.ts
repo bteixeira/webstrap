d74c6fc29716e3a296edb1392cf9a4ca6c49ac3d
Bruno Teixeira <bteixeira666@gmail.com>
2019-07-18 14:11:45 +0200

    Added default error logging
    Also added type annotations for NextFunction

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,7 +1,8 @@
 /* Imports */
 import * as path from 'path'
 import * as express from 'express'
-import {Request, Response} from 'express'
+import {Request, Response, NextFunction} from 'express'
+import Logger = require('bunyan')
 
 import defaultRouter from './routers/defaultRouter'
 import * as glue from './glue'
@@ -16,10 +17,16 @@ var n = 10000 /* Counter for request id */
 
 /* Middleware */
 app.use(express.json())
-app.use((request: Request, response: Response, next) => {
+app.use((request: Request, response: Response, next: NextFunction) => {
 	const logger = glue.logger.child({requestId: n++})
 	logger.info(request.method, request.path)
 	response.locals.logger = logger
+	next()
+})
+
+/* Error handling */
+app.use((error: any, request: Request, response: Response, next: NextFunction) => {
+	(response.locals.logger as Logger).error(`(${response.statusCode}) ${error}`)
 	next()
 })
 
